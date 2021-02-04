67f3e7569f588b498da246e2e54605ad535d6f98
najraham <anishamaharjan4@gmail.com>
2021-02-04 17:16:03 +0545

    used socket

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('./middlewares/cors');
-
 const app = express();
 
 // CORS 
+const cors = require('./middlewares/cors');
 app.use(cors)
 
 // database
@@ -23,10 +22,19 @@ con.on('open', () => {
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+// API
 // Auth
 app.use ('/api', require('./routes/auth'));
-
 // Messages
 app.use ('/api', require('./routes/message'));
 
-app.listen(5000, () => console.log('Server started on port 5000'));
+const server = app.listen(5000, () => console.log('Server started on port 5000'));
+
+const io = require('socket.io')(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+require('./socket')(io);
